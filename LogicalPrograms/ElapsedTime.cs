5e064f61b3f7dca6b4acbd0cc7f924f6250adbc8
[Sona] <[sona16061999@gmail.com]>
2021-07-02 22:46:37 +0530

    Calculate the elapsed Time

---
+++ LogicalPrograms/ElapsedTime.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    class ElapsedTime
+    {
+        
+           private static void StopWatch()
+            {
+            //
+            ElapsedTime stopwatch = new ElapsedTime();
+
+            //stopwatch started
+            stopwatch.Start();
+
+
+            //stopwatch stoped
+            stopwatch.Stop();
+
+            //timespan used to store time intervals
+            TimeSpan ts = stopwatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+
+            
+            
+        }
+    }
+}
+
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace LogicalPrograms
+{
+    class StopWatch
+    {
+        public static void Main()
+        {
+            StopWatch.ReadInput();
+            Console.ReadLine();
+        }
+
+
+        public static void ReadInput()
+        {
+            StartStopWatch();
+        }
+
+        private static void StartStopWatch()
+        {
+            //instance for stop watch
+            Stopwatch stopwatch = new Stopwatch();
+
+            //stopwatch start
+            stopwatch.Start();
+
+            
+            //stopwatch stop
+            stopwatch.Stop();
+
+            //timespan used to store time intervals
+            TimeSpan ts = stopwatch.Elapsed;
+
+            //printing using string interpolation
+            Console.WriteLine($"Time Elapsed {ts.Hours} hrs {ts.Minutes} mins {ts.Seconds} secs {ts.Milliseconds} ms");
+
+        }
+    }
+}
